894403a7af8454f7fe9a33687087bdf9a5956bd5
stefantewes (Stefan Tewes) <tewes_stefan@yahoo.de>
2009-12-01 14:42:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- rastamat-r/R/invaudspec.R
+++ rastamat-r/R/invaudspec.R
@@ -1,9 +1,38 @@
-# In Development
-invaudspec <- function(aspectrum, sr = 16000, nfft = 512, fbtype = 'bark', minfreq = 0, maxfreq = -1, sumpower = 1, bdwidth = 1){
+# Not Yet Tested
+invaudspec <- function(aspectrum, sr = 16000, nfft = 512, fbtype = "bark", minfreq = 0, maxfreq = -1, sumpower = TRUE, bdwidth = 1){
 
   if (maxfreq == -1) {
     maxfreq <- sr/2
   }
 
-  # return(list(spec=spec, wts=wts, iwts=iwts))
+  nfilts  <- nrow(aspectrum)
+  nframes <- ncol(aspectrum)
+
+  if (fbtype == "bark") {
+    wts <- fft2barkmx(nfft, sr, nfilts, bwidth, minfreq, maxfreq)
+  } else if (fbtype == "mel") {
+    wts <- fft2melmx(nfft, sr, nfilts, bwidth, minfreq, maxfreq)
+  } else if (fbtype == "htkmel") {
+    wts <- fft2melmx(nfft, sr, nfilts, bwidth, minfreq, maxfreq, 1, 1)
+  } else if (fbtype == "fcmel") {
+    wts <- fft2melmx(nfft, sr, nfilts, bwidth, minfreq, maxfreq, 1)
+  } else {
+    stop("fbtype", fbtype, "not recognized")
+  }
+ 
+  # Cut off 2nd half
+  wts <- wts[,1:((nfft/2)+1)]
+
+  # Just transpose, fix up 
+  ww   <- t(wts)%*%wts
+  iwts <- t(wts)/kronecker(t(rep(1, nfilts)), t(max(mean(diag(ww))/100, sum(ww))))
+
+  # Apply weights
+  if (sumpower) {
+    spec <- iwts %*% aspectrum
+  } else {
+    spec <- (iwts %*% sqrt(aspectrum))^2
+  }
+
+  return(list(spec=spec, wts=wts, iwts=iwts))
 }
