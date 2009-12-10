7f429c9ff7dfff6517337b4f3aedd517b79343d8
stefantewes (Stefan Tewes) <tewes_stefan@yahoo.de>
2009-12-10 14:27:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- rastamat-r/R/invpostaud.R
+++ rastamat-r/R/invpostaud.R
@@ -1,5 +1,38 @@
 # In Development
-invpostaud <- function(y, fmax, fbtype = 'bark', broaden = 0){
+invpostaud <- function(y, fmax, fbtype = "bark", broaden = 0){
 
-  # return(list(x=x, eql=eql))
+nbands  <- nrow(y)
+nframes <- ncol(y)
+
+if (fbtype == "bark") {
+  bandcfhz <- bark2hz(seq(0, hz2bark(fmax), length.out=nbands))
+} else if (fbtype == "mel") {
+  bandcfhz <- mel2hz(seq(0, hz2mel(fmax), length.out=nbands))
+} else if ((fbtype == "htkmel") || (fbtype == "fcmel")) {
+  bandcfhz <- mel2hz(seq(0, hz2mel(fmax,1), length.out=nbands),1)
+} else {
+  stop("unknown fbtype ", fbtype)
+}
+
+# Remove extremal bands (the ones that got duplicated)
+bandcfhz <- bandcfhz[(1+broaden):(nbands-broaden)]
+
+# Hynek's magic equal-loudness-curve formula
+fsq  <- bandcfhz^2
+ftmp <- fsq + 1.6e5
+eql  <- ((fsq/ftmp)^2) * ((fsq + 1.44e6)/(fsq + 9.61e6))
+
+# cube expand
+x <- y ^ (1/.33)
+
+# squash the zero in the eql curve
+if (eql[1] == 0) {  # or maybe always
+  eql[1]           <- eql[2]
+  eql[length(eql)] <- eql[length(eql)-1]
+}
+
+# weight the critical bands
+x <- x[(1+broaden):(nbands-broaden),] / kronecker(t(rep(1, nframes)), t(Conj(eql)))
+
+  return(list(x=x, eql=eql))
 }
