b61ab44c56b0d647de82aba7f5f7b8777522e9cb
stefantewes (Stefan Tewes) <tewes_stefan@yahoo.de>
2009-12-23 14:28:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- rastamat-r/R/cep2spec.R
+++ rastamat-r/R/cep2spec.R
@@ -1,6 +1,46 @@
-# In Development
+# Not Yet Tested
 cep2spec <- function(cep, nfreq=21, type=2){
 
-  # return(list(spec=spec, idctm=idctm))
+  ncep <- nrow(cep)
+  ncol <- ncol(cep)
+
+  # Make the DCT matrix
+  dctm  <- matrix(rep(0, ncep*nfreq), ncol=col)
+  idctm <- t(dctm)
+
+  if ((type == 2) || (type == 3)) {
+    # this is the orthogonal one, so inv matrix is same as fwd matrix
+    for (i in 1:ncep) {
+      dctm[i,] <- cos((i-1) * seq(1, (2*nfreq-1), 2) / (2*nfreq) * pi) * sqrt(2/nfreq)
+    }
+    if (type == 2) {
+      # make it unitary! (but not for HTK type 3)
+      dctm[1,] <- dctm[1,]/sqrt(2)
+    } else {
+      dctm[1,] <- dctm[1,]/2
+    }
+    idctm <- t(Conj(dctm))
+  } else if (type == 4) {
+    # type 1 with implicit repetition of first, last bins
+    # so all we do is reconstruct the middle nfreq rows of an nfreq+2 row idctm
+    for (i in 1:ncep) {
+      # 2x to compensate for fact that only getting +ve freq half
+      idctm[,i] <- 2*cos((i-1) * t(1:nfreq) / (nfreq+1) * pi)
+    }
+    # fixup 'non-repeated' basis fns 
+    idctm[, c(1, ncep)] <- idctm[, c(1, ncep)]/2
+  } else { 
+    # dpwe type 1 - idft of cosine terms
+    for (i in 1:ncep) {
+      # 2x to compensate for fact that only getting +ve freq half
+      idctm[,i] <- 2*cos((i-1) * t(0:(nfreq-1)) / (nfreq-1) * pi)
+    }
+    # fixup 'non-repeated' basis fns 
+    idctm[, c(1, ncep)] <- idctm[, c(1, ncep)]/2
+  }
+
+  spec <- exp(idctm*cep)
+
+  return(list(spec=spec, idctm=idctm))
 }
 
