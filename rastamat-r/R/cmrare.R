a05ba03a3c4018c6589e41833f6d6355b5e42df4
skrey (Sebastian Krey) <krey@statistik.tu-dortmund.de>
2010-06-13 17:46:14 +0000

    Add R.Martin/A.Nagathil CMRARE features
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ rastamat-r/R/cmrare.R
@@ -0,0 +1,43 @@
+cmrare <- function(x, v1=c(1,2), v2=c(0,8), p=3, qmax=512, ...){
+  if(is(x, "Wave")){
+    x  <- spectempevo(x, ...)
+  }
+
+  dimx <- dim(x)
+
+  if(!(is(x, "array") && length(dimx) == 3))
+    stop("'x' has to be of class 'Wave' or a 3-dimensional array")
+
+  xs <- sapply(seq(dimx[1]), function(y) apply(x[y,,], 1, sum))
+
+  qq <- qr(outer(seq(qmax), 0:p, "^"))
+
+  cmrare.fit <- function(n1, n2){
+    if(n2 == 0)
+      y <- xs[seq(qmax),n1+1]/xs[seq(qmax),1]
+    else
+      y <- apply(xs[seq(qmax),(n1:n2)+1, drop=FALSE], 1, sum)/( (n2-n1+1) * xs[seq(qmax),1])
+    return(qr.coef(qq, y))
+  }
+
+  b <- mapply(cmrare.fit, v1, v2)
+
+  return(b)
+}
+
+require(tuneR)
+a <- readWave("~/documents/Uni/Daten/McGill/piano-pl/pip_g4.wav")
+
+#for(i in list.files("~/documents/Uni/Software/rastamat-r/R")) source(paste("~/documents/Uni/Software/rastamat-r/R/", i, sep=""))
+setwd("~/documents/Uni/Software/tuner/rastamat-r/R")
+source("powspec.R")
+source("specgram.R")
+source("windowfunctions.R")
+source("spectempevo.R")
+
+print(system.time(stevos2 <- spectempevo(a)))
+print(gc())
+save(stevos2, file="~/stevos2.Rdata")
+
+
+#Olaf: d <- qr(A); c1 <- qr.coef(d, y1); c2 <- qr.coef(d, y2) sollte dann die schnellste möglichtkeit sein.
---
+++ rastamat-r/R/spectempevo.R
@@ -0,0 +1,42 @@
+spectempevo <- function(samples, sr=samples@samp.rate, wintime=0.032, hoptime=0.004,
+            K=16, S=1, preemph=0, dither=FALSE){
+
+    if(!is(samples, "Wave")) 
+        stop("'samples' needs to be of class 'Wave'")
+    validObject(samples)
+
+    if(samples@stereo) 
+        stop("Stereo processing not yet implemented...")
+
+    if(preemph != 0){
+        ssamples <- filter(samples@left, filter=c(1, -preemph), method="convolution", sides=1, circular=FALSE)
+    } else {
+        ssamples <- samples@left
+    }
+
+    # Compute FFT power spectrum
+    pspectrum <- powspec(ssamples, sr, wintime, hoptime, dither)
+    dpsp <-dim(pspectrum)
+
+    # Cepstra via FFT
+    # ps <- planFFT(dpsp[1])
+    # cepstra <- apply(S, 2, function(x) IFFT(x, plan=ps))
+    # cepstra <- apply(pspectrum, 2, function(x) fft(log(x), inverse=TRUE)/length(x))
+    cepstra <- mvfft(log(pspectrum), inverse=TRUE)/matrix(rep(dpsp[1], prod(dpsp)), nrow=dpsp[1])
+
+    lambda <- seq(1, (dpsp[2]-K+1), by=S)
+    llambda <- length(lambda)
+
+    # stevo <- array(mapply(function(x,y) abs(fft(cepstra[y,x:(x+K-1)])), rep(lambda, each=dpsp[1]), seq(dpsp[1])), c(K, dpsp[1], llambda))
+    # stevo <- array(sapply(lambda, function(x) abs(mvfft(t(cepstra[,x:(x+K-1)])))), c(K, dpsp[1], llambda))
+    # stevo <- sapply(lambda, function(x) abs(mvfft(t(cepstra[,x:(x+K-1)]))))
+    # dim(stevo) <- c(K, dpsp[1], llambda)
+    stevo <- array(numeric(K*dpsp[1]*llambda), c(K, dpsp[1], llambda))
+    for(i in seq(lambda)){
+      # stevo[,,i] <- apply(cepstra[,lambda[i]:(lambda[i]+K-1)], 1, function(x) abs(fft(x)))
+      stevo[,,i] <- abs(mvfft(t(cepstra[,lambda[i]:(lambda[i]+K-1)])))
+    }
+    
+    return(stevo)
+}
+
