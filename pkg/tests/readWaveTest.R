7d84d6472c4a19bf778d0f4f0fd25b47e89fbd8e
schnackenberg (Sarah Schnackenberg) <sarah.schnackenberg@tu-dortmund.de>
2013-07-10 06:48:46 +0000

    some tests for readWave
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/readWaveTest.R
+++ pkg/tests/readWaveTest.R
@@ -1,3 +1,41 @@
 library("tuneR")
-(x <- readWave("Testfiles/8bit_PCM_mono.wav"))
-x@left[1:10]
+(x1 <- readWave("Testfiles/8bit_PCM_mono.wav"))
+x1@left[1:10]
+(x2 <- readWave("Testfiles/8bit_PCM_stereo.wav"))
+x2@left[1:10]
+x2@right[1:10]
+(x3 <- readWave("Testfiles/16bit_PCM_mono.wav"))
+x3@left[1:10]
+(x4 <- readWave("Testfiles/16bit_PCM_stereo.wav"))
+x4@left[1:10]
+x4@right[1:10]
+(x5 <- readWave("Testfiles/24bit_PCM_mono.wav"))
+x5@left[1:10]
+(x6 <- readWave("Testfiles/24bit_PCM_mono_ex.wav"))
+x6@left[1:10]
+(x7 <- readWave("Testfiles/24bit_PCM_stereo.wav"))
+x7@left[1:10]
+x7@right[1:10]
+(x8 <- readWave("Testfiles/24bit_PCM_stereo_ex.wav"))
+x8@left[1:10]
+x8@right[1:10]
+(x9 <- readWave("Testfiles/32bit_float_mono.wav"))
+x9@left[1:10]
+(x10 <- readWave("Testfiles/32bit_float_stereo.wav"))
+x10@left[1:10]
+x10@right[1:10]
+(x12 <- readWave("Testfiles/32bit_PCM_mono.wav"))
+x12@left[1:10]
+(x13 <- readWave("Testfiles/32bit_PCM_mono_ex.wav"))
+x13@left[1:10]
+(x14 <- readWave("Testfiles/32bit_PCM_stereo.wav"))
+x14@left[1:10]
+x14@right[1:10]
+(x15 <- readWave("Testfiles/32bit_PCM_stereo_ex.wav"))
+x15@left[1:10]
+x15@right[1:10]
+(x16 <- readWave("Testfiles/64bit_float_mono.wav"))
+x16@left[1:10]
+(x17 <- readWave("Testfiles/64bit_float_stereo.wav"))
+x17@left[1:10]
+x17@right[1:10]
