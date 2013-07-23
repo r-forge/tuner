b79f8c6af4679365086c6dd4a5784f9b87923848
ligges (Uwe Ligges) <Uwe.Ligges@R-project.org>
2013-07-23 15:41:05 +0000

    length method for WaveMC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@124 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/length.R
+++ pkg/R/length.R
@@ -3,9 +3,9 @@ function(x){
     validObject(x)
     length(x@left)
 })
-#
-#setMethod("length", signature(x = "WaveMC"), 
-#function(x){
-#    validObject(x)
-#    nrow(x@.Data)
-#})
+
+setMethod("length", signature(x = "WaveMC"), 
+function(x){
+    validObject(x)
+    nrow(x@.Data)
+})
