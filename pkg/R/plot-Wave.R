e7687197de16f1c420cf18b066f9e9c15d76dcc0
schnackenberg (Sarah Schnackenberg) <sarah.schnackenberg@tu-dortmund.de>
2013-08-15 08:25:54 +0000

    plot for WaveMC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@138 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot-Wave.R
+++ pkg/R/plot-Wave.R
@@ -88,3 +88,110 @@ function(x, info = FALSE, xunit = c("time", "samples"),
             side = 1, outer = TRUE, line = if(stereo) 5 else 0, ...)
     }
 })
+
+plot.WaveMC.channel <- 
+function(x, xunit, ylim, xlab, ylab, main, nr, simplify, axes = TRUE, yaxt = par("yaxt"), ...){
+    null <- if(x@bit == 8) 128 else 0
+    l <- nrow(x)
+    if(all(ylim <= 0)) {
+      at <- pretty(x@.Data[,1])
+    } else {
+      at <- round((ylim[2] - null) * 2/3, -floor(log(ylim[2], 10)))
+      at <- null + c(-at, 0, at)
+    }
+    if(simplify && (l > nr)){
+        nr <- ceiling(l / round(l / nr))
+        index <- seq(1, l, length = nr)
+        if(xunit == "time") index <- index / x@samp.rate
+        mat <- matrix(c(x@.Data[,1], if(l %% nr > 0) rep(null, nr - (l %% nr))), 
+            nrow = nr, byrow = TRUE)
+        rg <- apply(mat, 1, range)
+        plot(index, rg[1,], type = "n", yaxt = "n", ylim = ylim, 
+            xlab = xlab, ylab = ylab, main = main, axes = axes, ...)
+        segments(index, rg[1,], index, rg[2,], ...)
+    }
+    else{
+        index <- seq(along = x@.Data[,1])
+        if(xunit == "time") index <- index / x@samp.rate
+        
+        plot(index, x@.Data[,1],
+            type = "l", yaxt = "n", ylim = ylim, xlab = xlab, 
+            ylab = ylab, main = main, axes = axes, ...)
+    }
+    if(axes) axis(2, at = at, yaxt = yaxt)
+}
+
+
+setMethod("plot", signature(x = "WaveMC", y = "missing"),
+function(x, info = FALSE, xunit = c("time", "samples"), 
+    ylim = NULL, main = NULL, sub = NULL, xlab = NULL, ylab = NULL, 
+    simplify = TRUE, nr = 1500, axes = TRUE, yaxt = par("yaxt"), mfrow = NULL, ...){
+    
+    xunit <- match.arg(xunit)
+    if(is.null(xlab)) xlab <- xunit
+    l <- nrow(x)
+    if(is.null(ylim)){
+        ylim <- range(x)
+        if(x@bit == 8)
+            ylim <- c(-1, 1) * max(abs(ylim - 127)) + 127
+        else
+            ylim <- c(-1, 1) * max(abs(ylim))
+    }
+    if(is.null(mfrow)) {
+      mfrow <- switch(ncol(x),
+        "2" = c(2,1),
+        "3" = c(3,1),
+        "4" = c(4,1),
+        "5" = c(5,1),
+        "6" = c(3,2),
+        "7" = c(4,2),
+        "8" = c(4,2),
+        "9" = c(3,3),
+        "10" = c(4,3),
+        "11" = c(4,3),
+        "12" = c(4,3),
+        "13" = c(5,3),
+        "14" = c(5,3),
+        "15" = c(5,3),
+        "16" = c(4,4),
+        "17" = c(5,4),
+        "18" = c(5,4))
+    }
+      
+    if(ncol(x)==1) {
+      if(info){
+              opar <- par(oma = c(2, 0, 0, 0))
+              on.exit(par(opar))
+      }
+      plot.WaveMC.channel(x, xunit = xunit, 
+          ylab = if(is.null(ylab)) "" else ylab,
+          main = main, sub = sub, xlab = xlab, ylim = ylim,
+          simplify = simplify, nr = nr, 
+          axes = axes, yaxt = yaxt, ...)
+    } else {
+      opar <- par(mfrow = mfrow, oma = c(if(info) 6.1 else 5.1, 0, 4.1, 0))
+      on.exit(par(opar))
+      mar <- par("mar")
+      par(mar = c(0, mar[2], 0, mar[4]))
+      for(i in 1:ncol(x)) {
+        plot.WaveMC.channel(x[,i], xunit = xunit,
+            ylab = if(is.null(ylab)) paste("channel ", i) else ylab,         
+            main = NULL, sub = if(i %in% (ncol(x)-0:(mfrow[2]-1))) sub else NULL, xlab = NULL, ylim = ylim,   
+            xaxt = if(i %in% (ncol(x)-0:(mfrow[2]-1))) "s" else "n", simplify = simplify, nr = nr, 
+            axes = axes, yaxt = yaxt, ...)
+      }
+      title(main = main, outer = TRUE, line = 2)
+      title(xlab = xlab, outer = TRUE, line = 3)
+      title(sub  = sub , outer = TRUE, line = 4)
+      par(mar = mar)
+    }
+    if(info){
+        mtext(paste("WaveMC Object: ",  
+                l, " samples (", 
+                round(l / x@samp.rate, 2),  " sec.), ",
+                x@samp.rate, " Hertz, ",
+                x@bit, " bit, ",
+                ncol(x), " Channels."), 
+            side = 1, outer = TRUE, line = if(ncol(x)==1) 0 else 5, ...)      #####
+    }
+})
