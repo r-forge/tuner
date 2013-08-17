5a2e1478415bec1b276de0ac5f06fb8326dd84fd
ligges (Uwe Ligges) <Uwe.Ligges@R-project.org>
2013-08-17 23:09:02 +0000

    Add internal ToDo list
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/ToDo.R
@@ -0,0 +1,30 @@
+library("tuneR", lib.loc="d:/temp/")
+x6 <- readWave("d:/waves/6_Channel_ID.wav")
+x1 <- readWave("D:/uwe/R/library-devel/tuneR/pkg/tests/Testfiles/8bit_PCM_mono.wav")
+x2 <- readWave("D:/uwe/R/library-devel/tuneR/pkg/tests/Testfiles/16bit_PCM_stereo.wav")
+
+#setwd("D:/uwe/R/library-devel/tuneR/pkg/tests")
+
+R/periodogram.R:    if(!is(object, "Wave"))
+R/Wspec.R:    cat("Properties of the Wave object:\n")
+
+- WaveMC-class: neu
+- WaveMC: neu
+-> See also: auf vielen Seiten ergänzen
+- WaveMC-method
+
+- Arith-method: WaveMC Klasse
+- channel, mono: Verweis auf "[" Methode zur Selektion bei WaveMC.
+- coerce-method: Auch für WaveMC
+- downsample
+- melfcc
+- MFCC: remove?
+- Waveforms
+- normalize
+- noSilence
+- periodogram?
+- plot
+- readWave (WaveIO)
+- tuneR: WaveMC einbringen
+- writeWave (WaveIO)
+- MCnames
