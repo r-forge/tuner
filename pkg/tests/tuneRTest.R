775dafb9810ad088f909975e3d2e45b540fe1c5a
schnackenberg (Sarah Schnackenberg) <sarah.schnackenberg@tu-dortmund.de>
2013-08-27 18:20:56 +0000

    improve tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tuner@177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/tuneRTest.R
+++ pkg/tests/tuneRTest.R
@@ -1,120 +1,140 @@
 library("tuneR")
 library("tools")
 
-## writeWave
+x1 <- sine(660, pcm=TRUE, bit=8, duration=500)
+x2 <- sine(440, pcm=TRUE, bit=8, duration=500)
+x3 <- sine(220, pcm=TRUE, bit=8, duration=500)
+
+## writeWave (always Wave extensible format)
 files <- tempfile(as.character(1:100), fileext = ".wav")
-## 8bit
-writeWave(sine(440, pcm=TRUE, bit=8), file = files[1]) 
-writeWave(stereo(sine(440, bit=8, pcm=TRUE), sine(220, bit=8, pcm=TRUE)), file = files[2])
-## 16bit
-writeWave(sine(440, pcm=TRUE, bit=16), file = files[3])
-writeWave(stereo(sine(440, bit=16, pcm=TRUE), sine(220, bit=16, pcm=TRUE)), file = files[4])
-## 24bit
-writeWave(sine(440, pcm=TRUE, bit=24), file = files[5])
-writeWave(stereo(sine(440, bit=24, pcm=TRUE), sine(220, bit=24, pcm=TRUE)), file = files[6])
+## 8bit (pcm)
+## - mono
+writeWave(x2, file = files[1]) 
+## - stereo
+writeWave(stereo(x2, x3), file = files[2])
+## 16bit (pcm)
+## - mono
+writeWave(sine(440, pcm=TRUE, bit=16, duration=500), file = files[3])
+## - stereo
+writeWave(stereo(sine(440, bit=16, pcm=TRUE, duration=500), sine(220, bit=16, pcm=TRUE, duration=500)), file = files[4])
+## 24bit (pcm)
+## - mono
+writeWave(sine(440, pcm=TRUE, bit=24, duration=500), file = files[5])
+## - stereo
+writeWave(stereo(sine(440, bit=24, pcm=TRUE, duration=500), sine(220, bit=24, pcm=TRUE, duration=500)), file = files[6])
 ## 32bit
-sine32 <- sine(440, bit=32)
+sine32 <- sine(440, bit=32, duration=500)
 sine32@left <- round(sine32@left, 5)
+## - IEEE_FLOAT mono
 writeWave(sine32, file = files[7])
-writeWave(sine(440, pcm=TRUE, bit=32), file = files[8])
+## - pcm mono
+writeWave(sine(440, pcm=TRUE, bit=32, duration=500), file = files[8])
+## - IEEE_FLOAT stereo
 writeWave(stereo(sine32, sine32), file = files[9])
-writeWave(stereo(sine(440, bit=32, pcm=TRUE), sine(220, bit=32, pcm=TRUE)), file = files[10])
-## 64bit
+## - pcm stereo
+writeWave(stereo(sine(440, bit=32, pcm=TRUE, duration=500), sine(220, bit=32, pcm=TRUE, duration=500)), file = files[10])
+## 64bit (IEEE_FLOAT)
 exact64 <- c(-2^(-40:0), 2^(-40:0))
+## - mono
 writeWave(Wave(left = exact64, bit = 64, pcm = FALSE, samp.rate = 44100), file = files[11])
+## - stereo
 writeWave(Wave(left = exact64, right = exact64, bit = 64, pcm = FALSE, samp.rate = 44100), file = files[12])
-as.vector(md5sum(files[1:12]))
+## multi channel
+mc <- WaveMC(cbind(x1@left, x2@left, x3@left), bit = 16, samp.rate = 44100)
+colnames(mc) <- c("FL","FR","FC")
+writeWave(mc, file = files[13]) 
+colnames(mc) <- c("FL","FC","FR")
+writeWave(mc, file = files[14])
+as.vector(md5sum(files[1:14]))
 
 ## Waveforms
 ## sine
-(x1 <- sine(440, pcm=TRUE, bit=8))
-length(x1@left)
-x1@stereo
-x1@samp.rate
-x1@bit
-x1@pcm
-(x2 <- stereo(sine(440, bit=8, pcm=TRUE), sine(220, bit=8, pcm=TRUE)))
+(x2)
 length(x2@left)
 x2@stereo
 x2@samp.rate
 x2@bit
 x2@pcm
-(x3 <- sine(440, bit=32, duration = 1000))
-length(x3@left)
-x3@stereo
-x3@samp.rate
-x3@bit
-x3@pcm
-(x4 <- sine(440, bit=8, pcm=TRUE, samp.rate=11025))
+(x4 <- stereo(x2, x3))
 length(x4@left)
 x4@stereo
 x4@samp.rate
 x4@bit
-x4@pcm 
-(x5 <- sine(440, bit=8, pcm=TRUE, stereo = TRUE))
+x4@pcm
+(x5 <- sine(440, bit=32, duration = 500))
 length(x5@left)
 x5@stereo
 x5@samp.rate
 x5@bit
 x5@pcm
+(x6 <- sine(440, bit=8, pcm=TRUE, samp.rate=11025, duration=500))
+length(x6@left)
+x6@stereo
+x6@samp.rate
+x6@bit
+x6@pcm 
+(x7 <- sine(440, bit=8, pcm=TRUE, stereo = TRUE, duration=500))
+length(x7@left)
+x7@stereo
+x7@samp.rate
+x7@bit
+x7@pcm
 
 ## normalize
-obj <- sine(440, pcm=TRUE, bit=8)
-obj2 <- stereo(sine(440, bit=8, pcm=TRUE), sine(220, bit=8, pcm=TRUE))
-(x6 <- normalize(obj, unit = "1", center = TRUE, level = 1, rescale = TRUE))
-x6@left[1:10]
-(x7 <- normalize(obj, unit = "1", center = TRUE, level = 1, rescale = FALSE))
-x7@left[1:10]
-(x8 <- normalize(obj, unit = "8", center = TRUE, level = 2, rescale = TRUE))
-x8@left[1:10]
-(x9 <- normalize(obj, unit = "8", center = TRUE, level = 2, rescale = FALSE))
-x9@left[1:10]
-(x10 <- normalize(obj, unit = "16", center = TRUE, level = 1, rescale = TRUE)) 
+(x8 <- normalize(x1, unit = "1", center = TRUE, level = 1, rescale = TRUE))
+round(x8@left[1:10], 5)
+(x9 <- normalize(x1, unit = "1", center = TRUE, level = 1, rescale = FALSE))
+round(x9@left[1:10], 5)
+(x10 <- normalize(x1, unit = "8", center = TRUE, level = 2, rescale = TRUE))
 x10@left[1:10]
-(x12 <- normalize(obj, unit = "16", center = TRUE, level = 1, rescale = FALSE)) 
+(x12 <- normalize(x1, unit = "8", center = TRUE, level = 2, rescale = FALSE))
 x12@left[1:10]
-(x13 <- normalize(obj, unit = "24", center = TRUE, level = 1, rescale = TRUE))
+(x13 <- normalize(x1, unit = "16", center = TRUE, level = 1, rescale = TRUE)) 
 x13@left[1:10]
-(x14 <- normalize(obj, unit = "24", center = TRUE, level = 1, rescale = FALSE))
+(x14 <- normalize(x1, unit = "16", center = TRUE, level = 1, rescale = FALSE)) 
 x14@left[1:10]
-(x15 <- normalize(obj, unit = "32", center = TRUE, level = 1, rescale = TRUE))
+(x15 <- normalize(x1, unit = "24", center = TRUE, level = 1, rescale = TRUE))
 x15@left[1:10]
-(x16 <- normalize(obj, unit = "32", center = TRUE, level = 1, rescale = FALSE))
+(x16 <- normalize(x1, unit = "24", center = TRUE, level = 1, rescale = FALSE))
 x16@left[1:10]
-(x17 <- normalize(obj, unit = "32", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+(x17 <- normalize(x1, unit = "32", center = TRUE, level = 1, rescale = TRUE))
 x17@left[1:10]
-(x18 <- normalize(obj, unit = "32", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+(x18 <- normalize(x1, unit = "32", center = TRUE, level = 1, rescale = FALSE))
 x18@left[1:10]
-(x19 <- normalize(obj, unit = "64", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
-x19@left[1:10]
-(x20 <- normalize(obj, unit = "64", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
-x20@left[1:10]
-(x21 <- normalize(obj2, unit = "1", center = TRUE, level = 1, rescale = TRUE))
-x21@left[1:10]
-x21@right[1:10]
-(x22 <- normalize(obj2, unit = "1", center = TRUE, level = 1, rescale = FALSE))
-x22@left[1:10]
-(x23 <- normalize(obj2, unit = "8", center = TRUE, level = 2, rescale = TRUE))
-x23@left[1:10]
-(x24 <- normalize(obj2, unit = "8", center = TRUE, level = 2, rescale = FALSE))
-x24@left[1:10]
-(x25 <- normalize(obj2, unit = "16", center = TRUE, level = 1, rescale = TRUE)) 
+(x19 <- normalize(x1, unit = "32", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+round(x19@left[1:10], 5)
+(x20 <- normalize(x1, unit = "32", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+round(x20@left[1:10], 5)
+(x21 <- normalize(x1, unit = "64", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+round(x21@left[1:10], 5)
+(x22 <- normalize(x1, unit = "64", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+round(x22@left[1:10], 5)
+(x23 <- normalize(sine(440, bit=8, pcm=TRUE, stereo = TRUE, duration=500), unit = "1", center = TRUE, level = 1, rescale = TRUE))
+round(x23@left[1:10], 5)
+round(x23@right[1:10], 5)
+(x24 <- normalize(x2, unit = "1", center = TRUE, level = 1, rescale = FALSE))
+round(x24@left[1:10], 5)
+(x25 <- normalize(x2, unit = "8", center = TRUE, level = 2, rescale = TRUE))
 x25@left[1:10]
-(x26 <- normalize(obj2, unit = "16", center = TRUE, level = 1, rescale = FALSE)) 
+(x26 <- normalize(x2, unit = "8", center = TRUE, level = 2, rescale = FALSE))
 x26@left[1:10]
-(x27 <- normalize(obj2, unit = "24", center = TRUE, level = 1, rescale = TRUE))
+(x27 <- normalize(x2, unit = "16", center = TRUE, level = 1, rescale = TRUE)) 
 x27@left[1:10]
-(x28 <- normalize(obj2, unit = "24", center = TRUE, level = 1, rescale = FALSE))
+(x28 <- normalize(x2, unit = "16", center = TRUE, level = 1, rescale = FALSE)) 
 x28@left[1:10]
-(x29 <- normalize(obj2, unit = "32", center = TRUE, level = 1, rescale = TRUE))
+(x29 <- normalize(x2, unit = "24", center = TRUE, level = 1, rescale = TRUE))
 x29@left[1:10]
-(x30 <- normalize(obj2, unit = "32", center = TRUE, level = 1, rescale = FALSE))
+(x30 <- normalize(x2, unit = "24", center = TRUE, level = 1, rescale = FALSE))
 x30@left[1:10]
-(x31 <- normalize(obj2, unit = "32", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+(x31 <- normalize(x2, unit = "32", center = TRUE, level = 1, rescale = TRUE))
 x31@left[1:10]
-(x32 <- normalize(obj2, unit = "32", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+(x32 <- normalize(x2, unit = "32", center = TRUE, level = 1, rescale = FALSE))
 x32@left[1:10]
-(x33 <- normalize(obj2, unit = "64", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
-x33@left[1:10]
-(x34 <- normalize(obj2, unit = "64", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
-x34@left[1:10]
+(x33 <- normalize(x2, unit = "32", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+round(x33@left[1:10], 5)
+(x34 <- normalize(x2, unit = "32", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+round(x34@left[1:10], 5)
+(x35 <- normalize(x2, unit = "64", center = TRUE, level = 1, rescale = TRUE, pcm = FALSE))
+round(x35@left[1:10], 5)
+(x36 <- normalize(x2, unit = "64", center = TRUE, level = 1, rescale = FALSE, pcm = FALSE))
+round(x36@left[1:10], 5)
